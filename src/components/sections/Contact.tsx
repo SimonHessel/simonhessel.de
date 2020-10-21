9a5ac188b8b69edfbfb63aecc74f2535d0be6f87
Simon Hessel <simon.hessel@kreios.lu>
2020-10-21 12:32:08 +0200

    updated contact form link generation

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useState } from "react";
 import useTranslation from "../../hooks/useTranslation";
 
 type Props = {};
@@ -9,32 +9,28 @@ const Contact = ({}: Props) => {
     [key: string]: string;
   }>("form");
 
-  const handleLogin = useCallback(async (event) => {
-    event.preventDefault();
-    const {
-      name: { value: name },
-      email: { value: email },
-      message: { value: message },
-    } = event.target.elements;
-    const link = `mailto:hessel.simon@gmail.com?body=${encodeURIComponent(
-      `${message}
-      
-      Regards ${name}
-      ${email}`
-    )}`;
-    window.open(link, "_blank");
-  }, []);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <section id="four" className="wrapper style2 special">
       <div className="inner">
         <header className="major narrow">
           <h2 id="contactHeader">{header}</h2>
         </header>
-        <form onSubmit={handleLogin}>
+        <form>
           <div className="container 75%">
             <div className="row uniform 50%">
               <div className="6u 12u$(xsmall)">
-                <input id="name" name="name" placeholder="Name" type="text" />
+                <input
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
               <div className="6u$ 12u$(xsmall)">
                 <input
@@ -42,6 +38,8 @@ const Contact = ({}: Props) => {
                   name="email"
                   placeholder="Email"
                   type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div className="12u$">
@@ -50,18 +48,25 @@ const Contact = ({}: Props) => {
                   name="message"
                   placeholder={messagePlaceholder}
                   rows={4}
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
                 ></textarea>
               </div>
             </div>
           </div>
           <ul className="actions">
             <li>
-              <input
-                id="send"
-                type="submit"
-                className="special"
-                value={sendBtn}
-              />
+              <a
+                className="button special"
+                href={`mailto:hessel.simon@gmail.com?body=${encodeURIComponent(
+                  `${message}
+                  
+                  Regards ${name}
+                  ${email}`
+                )}`}
+              >
+                {sendBtn}
+              </a>{" "}
             </li>
             <li>
               <input type="reset" className="alt" value={resetBtn} />
