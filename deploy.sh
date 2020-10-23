40ad4140540b3bbfb187192afe23752f96abf56f
Simon Hessel <simon.hessel@kreios.lu>
2020-10-23 20:22:56 +0200

    Added deploy bash script to npm script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+git pull
+git checkout gh-pages 
+git pull
+rm -rf .nojekyll
+git add .
+git commit -a -m "remove"
+git push origin gh-pages
+touch .nojekyll
+git add .
+git commit -a -m "add"
+git push origin gh-pages
